ecf9a18735e4c58d3f84cc87657eaa07dbd90ec0
sidneiali <sidneiali01@gmail.com>
2021-07-20 17:55:02 -0300

    projeto recriando a interface netflix

---
+++ README.md
@@ -0,0 +1,7 @@
+# NerdFlix
+## Recriando a Interface do Netflix
+
+Projeto criado para compor atividade de estudos HTML, CSS e Javascript
+
+![alt](/img/projeto.png)
+
---
+++ js/setup.js
@@ -0,0 +1,41 @@
+$('.owl-carouselP').owlCarousel({
+  loop:true,
+  margin:10,
+  loop:true,
+  nav:false,
+  autoplay:true,
+  autoplayTimeout:2000,
+  autoplayHoverPause:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:8
+      }
+  }
+})
+
+$('.owl-carouselS').owlCarousel({
+    loop:true,
+    margin:10,
+    loop:true,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:8
+        }
+    }
+  })
